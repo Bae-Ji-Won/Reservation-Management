ec1f379c93acc6057c9a9f0aba40d39de2e35b6a
Bae Ji Won <qowhxk@naver.com>
2024-04-24 15:53:59 +0900

    #1 User - Authentication Error Handler Add

---
+++ src/main/java/msa/reservation/exception/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,20 @@
+package msa.reservation.exception;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import msa.reservation.dto.response.Response;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import java.io.IOException;
+
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        response.setContentType("application/json");
+        response.setStatus(ErrorCode.INVALID_TOKEN.getStatus().value());
+        response.getWriter().write(Response.error(ErrorCode.INVALID_TOKEN.name()).toStream());
+    }
+}
