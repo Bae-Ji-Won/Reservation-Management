62832295bf179219505e1fee4a9d5816f7c48ddb
Bae Ji Won <qowhxk@naver.com>
2024-04-24 15:54:54 +0900

    Error Code

--- src/main/java/msa/reservation/exception/ErrorCode.java
+++ src/main/java/msa/reservation/exception/ErrorCode.java
@@ -10,9 +10,15 @@ public enum ErrorCode {
 
     USER_NOT_FOUND(HttpStatus.NOT_FOUND,"User Not Found"),
     DUPLICATED_USER_NAME(HttpStatus.CONFLICT,"User name is duplicated"),
+
+    EMAIL_NOT_FOUND(HttpStatus.NOT_FOUND,"Email Not Found"),
     DUPLICATED_EMAIL(HttpStatus.CONFLICT,"Email is duplicated"),
 
+    FORM_WRONG(HttpStatus.BAD_REQUEST, "Join Form is Wrong"),
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED,"Password is invalid"),
+
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "Token is valid"),
+
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error");
     private HttpStatus status;
     private String message;
